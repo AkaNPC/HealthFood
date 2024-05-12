f59d6082651af7ffe3f5c17cbf10dfae01090c97
Artur K <tornraxx@gmail.com>
2024-05-12 13:05:19 +0700

    Базовая страница для выбора меню здорового питания. Реализован индивидуальный калькулятор калорий, а также заполнение формы + модальное окно

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import cards from './modules/cards';
+import { openModal } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2023-01-11');
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        currentCounter: '#current',
+        totalCounter: '#total',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    calc();
+    cards();
+
+});
+
+
+
